f167a080bc4f8475fbcae768ddc7b8090bd7cb5e
Erik Ciau <erikciau777@icloud.com>
2022-10-05 17:50:20 -0500

    project configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,31 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { EmployeesModule } from './modules/employees/employees.module';
+import { ProductsModule } from './modules/products/products.module';
+import { OrdersModule } from './modules/orders/orders.module';
+import { CustomersModule } from './modules/customers/customers.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'erikciau',
+      password: 'root',
+      database: 'ararat-inventory',
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+    EmployeesModule,
+    ProductsModule,
+    OrdersModule,
+    CustomersModule,
+    AuthModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
