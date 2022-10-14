e7207ae8b98e6e5da849ac7ac6257ff3aba6fdaf
ErikCiauDev <erikciau777@icloud.com>
2022-10-14 00:52:13 -0500

    fix container di modules

--- src/modules/orders/orders.module.ts
+++ src/modules/orders/orders.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Stock, Variant } from '../products/models';
 import { OrderController } from './controllers/orders.controller';
 import { OrderLine } from './models/order-line.model';
 import { Order } from './models/order.model';
 import { OrdersService } from './services/orders.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order, OrderLine])],
+  imports: [TypeOrmModule.forFeature([Order, OrderLine, Variant, Stock])],
   controllers: [OrderController],
   providers: [OrdersService],
 })
